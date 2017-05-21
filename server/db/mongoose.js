bc2b10846a2f6d972e32ca0d825bfac5ae9f04ac
Onoufrios Fakelakis <chibioni@gmail.com>
2017-05-21 22:23:24 +0300

    Updated endpoints

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://pchalats:Panahs18s!@ds133291.mlab.com:33291/betserviceinfodb');
+mongoose.connect('mongodb://pchalats:Panahs18s!@ds133291.mlab.com:33291/betserviceinfodb');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost:27017/betserviceinfodb');
+//mongoose.connect('mongodb://localhost:27017/betserviceinfodb');
 
 module.exports = { mongoose };
